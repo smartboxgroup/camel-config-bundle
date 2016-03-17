901699dfaea936be5f019cadbeebd471b9f33d76
Jose Rufino <jose.rufino@smartbox.com>
2016-03-17 15:10:27 +0000

    Added new Interfaces and Classes and renamed ProducersRouter to EndpointRouter

--- DependencyInjection/FlowsBuilderCompilerPass.php
+++ DependencyInjection/FlowsBuilderCompilerPass.php
@@ -135,8 +135,8 @@ class FlowsBuilderCompilerPass implements CompilerPassInterface, FlowsBuilderInt
                     $definition->addMethodCall('setEventDispatcher', array(new Reference('event_dispatcher')));
                     break;
 
-                case 'Smartbox\Integration\FrameworkBundle\Traits\UsesProducersRouter':
-                    $definition->addMethodCall('setProducersRouter', array(new Reference('smartesb.router.producers')));
+                case 'Smartbox\Integration\FrameworkBundle\Traits\UsesEndpointRouter':
+                    $definition->addMethodCall('setEndpointsRouter', array(new Reference('smartesb.router.endpoints')));
                     break;
 
                 case 'Smartbox\Integration\FrameworkBundle\Traits\MessageFactoryAware':
